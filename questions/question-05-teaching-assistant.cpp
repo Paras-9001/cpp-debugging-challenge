98f75aa69441225b3561f1163515579a2a710f5f
Paras-9001 <parasghadge3443@gmail.com>
2026-08-13 17:59:06 +0530

    Fixed the diamond inheritance problem in the program
    1. Used the virtual base class concept to derive a class Student and Employee from class Person. Line-26,47
    2.  Used scope resolution operator (::) to call showRole() function- Line 95
    3. Removed unnecessary messages- Line- 17,37,58,87,88
    
    Conceptual Check Questions:
    1. Two Person objects are created in original inheritance structure because both Student and Employee class inherit from Person. When TeachingAssistant class is created, one object is used for student and other is used for Employee.
    
    2. Diamond Inheritance is a problem in which a class derived from two classes which are derived from same base class. Example- A->B,C->D here B and C is derived from A and D is derived from B and C
    
    3. Virtual inheritance can solves the problem as it ensures only one shared copy of the common base class exists and there is no duplicate copy.
    
    4. The keyword virtual should be added while deriving Student and Employee from Person.
    class Student : virtual public Person
    class Employee : virtual public Person

--- questions/question-05-teaching-assistant.cpp
+++ questions/question-05-teaching-assistant.cpp
@@ -14,7 +14,7 @@ public:
     Person(string n)
     {
         name = n;
-        cout << "Person created" << endl;
+
     }
 
     void showPerson()
@@ -23,7 +23,7 @@ public:
     }
 };
 
-class Student : public Person
+class Student : virtual public Person
 {
 protected:
     int rollNo;
@@ -34,7 +34,7 @@ public:
         : Person(n)
     {
         rollNo = r;
-        cout << "Student created" << endl;
+        
     }
 
     void showRole()
@@ -44,7 +44,7 @@ public:
     }
 };
 
-class Employee : public Person
+class Employee : virtual public Person
 {
 protected:
     int employeeId;
@@ -55,7 +55,7 @@ public:
         : Person(n)
     {
         employeeId = id;
-        cout << "Employee created" << endl;
+        
     }
 
     void showRole()
@@ -79,18 +79,20 @@ public:
         int r,
         int id,
         int h)
-        : Student(n, r),
+        : Person(n),
+          Student(n, r),
           Employee(n, id)
     {
         hours = h;
-        cout << "Teaching Assistant created"
-             << endl;
+       
+            
     }
 
     void display()
     {
         showPerson();
-        showRole();
+
+        Student::showRole();
 
         cout << "Working hours: "
              << hours << endl;
